8bc8a5b1c1e5a2e5d3389941460daf88a88e5e2b
Julianne Joswiak <juliannej@mac.com>
2016-11-16 11:39:06 +0000

    map working in development. need to make google developer account

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,6 +1,12 @@
 class SpacesController < ApplicationController
   def index
-    @spaces = Space.all
+    @spaces = Space.where.not(latitude: nil, longitude: nil)
+
+    @markers = Gmaps4rails.build_markers(@spaces) do |space, marker|
+      marker.lat space.latitude
+      marker.lng space.longitude
+      # marker.infowindow render_to_string(partial: "/spaces/map_box", locals: { space: space })
+    end
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115175846) do
+ActiveRecord::Schema.define(version: 20161116105506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,9 @@ ActiveRecord::Schema.define(version: 20161115175846) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "name"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.text     "description"
     t.index ["user_id"], name: "index_spaces_on_user_id", using: :btree
   end
 
