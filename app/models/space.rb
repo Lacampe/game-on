28abb81671a6530ceb3bd5d87037dc048e6873f9
Romain Campech <romain.campech@city.ac.uk>
2016-11-17 14:28:06 +0000

    Space and User reviews - model/controller/routes

--- app/models/space.rb
+++ app/models/space.rb
@@ -20,5 +20,7 @@ class Space < ApplicationRecord
   after_validation :geocode, if: :address_changed?
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_many :reviews, through: :bookings
+
   has_attachments :photos, maximum: 5
 end
