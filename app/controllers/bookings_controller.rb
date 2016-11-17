e28f70ddcd8982c5b83eb14874f857ec39bbbcc7
Julianne Joswiak <juliannej@mac.com>
2016-11-17 18:23:13 +0000

    no way to test this as it's for production... what a quandary... any ideas?

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,8 @@
 class BookingsController < ApplicationController
   before_action :find_space, only: [ :new, :create, :destroy ]
+  before_action :find_booking, only: [ :show, :destroy, :confirm]
 
   def show
-    @booking = Booking.find(params[:id])
     @space = Space.find(@booking.space_id)
     @user = User.find(@booking.user_id)
     @owner = User.find(@space.user_id)
@@ -17,18 +17,18 @@ class BookingsController < ApplicationController
     @booking.user = current_user
     @booking.price = (@space.price_per_hour.to_i * @booking.duration.to_i)
     @booking.save
+    UserNotifier.send_user_booking_confirmation_email(@booking).deliver
+    UserNotifier.send_owner_booking_confirmation_email(@booking).deliver
     redirect_to space_booking_path(@space, @booking)
   end
 
   def destroy
-    @booking = Booking.find(params[:id])
     @booking.destroy
     flash[:notice] = "Booking has been rejected"
     redirect_to root
   end
 
   def confirm
-    @booking = Booking.find(params[:id])
     @booking.confirmed = true
     @booking.save
     flash[:notice] = "Booking has been confirmed"
@@ -43,5 +43,9 @@ class BookingsController < ApplicationController
 
   def find_space
     @space = Space.find(params[:space_id])
+  end
+
+  def find_booking
+    @booking = Booking.find(params[:id])
   end
 end
