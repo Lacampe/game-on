420592ee06431dc7569993eb963b0eb560462503
Romain Campech <romain.campech@city.ac.uk>
2016-11-17 16:40:02 +0000

    Dropped space&user_reviews tables

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   mount Attachinary::Engine => "/attachinary"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :user_reviews, only: [ :index, :new, :create ]
-  end
+  resources :users
 
   resources :spaces do
     resources :bookings, except: [ :index, :edit, :update ]
-    resources :space_reviews, only: [ :index, :new, :create ]
   end
 end
