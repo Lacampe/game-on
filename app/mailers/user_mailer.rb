e3a8f324b35b88a770f0348f115d71d97d08d811
Julianne Joswiak <juliannej@mac.com>
2016-11-17 16:11:46 +0000

    mailers have o.k. messages now

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,10 @@
 class UserMailer < ApplicationMailer
   def welcome(user)
     @user = user  # Instance variable => available in view
-
+    @greeting = "Hello! I hope you enjoy quickly booking sports venues through game-on booking. Click to get started:"
+    @link_url = "https://game-on-le-wagon.herokuapp.com"
     mail(to: @user.email, subject: 'Welcome to Game On Booking')
+
     # This will render a view in `app/views/user_mailer`!
   end
 end
