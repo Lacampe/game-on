69333a470ceb3c7bc11e55d48d6cacaf93102b46
Julianne Joswiak <juliannej@mac.com>
2016-11-17 10:37:42 +0000

    model updated - todo: update booking form on space show page

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :space
+  validates :duration, :inclusion => {:in => (1..12)}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116105542) do
+ActiveRecord::Schema.define(version: 20161117103106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20161116105542) do
     t.integer  "space_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "duration"
     t.index ["space_id"], name: "index_bookings_on_space_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
