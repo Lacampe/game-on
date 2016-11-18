dccf0a66e1d42160c5b5a722c0f622273e597339
Julianne Joswiak <juliannej@mac.com>
2016-11-18 12:36:54 +0000

    typo fixed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,7 +10,7 @@ class UserMailer < ApplicationMailer
   def send_user_space_confirmation_email(user, space)
     @user = user
     @space = space
-    mail(to: @user.email, subject: 'Space confirmed', from: "Game-On")
+    mail(to: @user.email, subject: 'Space confirmed')
   end
 
   def send_user_booking_confirmation_email(booking, owner, user)
@@ -18,7 +18,7 @@ class UserMailer < ApplicationMailer
     @user = user
     @owner = owner
 
-    mail(to: @user.email, subject: 'You have requested a booking', from: "Game-On")
+    mail(to: @user.email, subject: 'You have requested a booking')
   end
 
   def send_owner_booking_confirmation_email(booking, owner, user)
@@ -26,20 +26,20 @@ class UserMailer < ApplicationMailer
     @owner = owner
     @user = user
 
-    mail(to: @user.email, subject: 'Someone has requested a booking', from: "Game-On")
+    mail(to: @user.email, subject: 'Someone has requested a booking')
   end
 
   def send_user_bookingconfirmed_confirmation_email(user, booking)
     @booking = booking
     @user = user
 
-    mail(to: @user.email, subject: 'Booking confirmed', from: "Game-On")
+    mail(to: @user.email, subject: 'Booking confirmed')
   end
 
   def send_user_bookingrejected_confirmation_email(user, booking)
     @booking = booking
     @user = user
 
-    mail(to: @user.email, subject: 'Booking rejected', from: "Game-On")
+    mail(to: @user.email, subject: 'Booking rejected')
   end
 end
