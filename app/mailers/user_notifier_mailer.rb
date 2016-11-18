659fab1cf7e4e4f1a896e5a0b81851f6e5b326e6
Julianne Joswiak <juliannej@mac.com>
2016-11-18 12:40:43 +0000

    typo

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,12 +1,12 @@
 class UserNotifierMailer < ApplicationMailer
   def send_signup_email(user)
     @user = user
-    mail(to: @user.email, subject: 'Welcome to Game-On')
+    mail(to: @user.email, subject: 'Welcome to Game-On', from: "Game-On")
   end
   def send_user_space_confirmation_email(user, space)
     @user = user
     @space = space
-    mail(to: @user.email, subject: 'Space confirmed')
+    mail(to: @user.email, subject: 'Space confirmed', from: "Game-On")
   end
 
   def send_user_booking_confirmation_email(booking, owner, user)
@@ -14,7 +14,7 @@ class UserNotifierMailer < ApplicationMailer
     @user = user
     @owner = owner
 
-    mail(to: @user.email, subject: 'You have requested a booking')
+    mail(to: @user.email, subject: 'You have requested a booking', from: "Game-On")
   end
 
   def send_owner_booking_confirmation_email(booking, owner, user)
@@ -22,21 +22,21 @@ class UserNotifierMailer < ApplicationMailer
     @owner = owner
     @user = user
 
-    mail(to: @user.email, subject: 'Someone has requested a booking')
+    mail(to: @user.email, subject: 'Someone has requested a booking', from: "Game-On")
   end
 
   def send_user_bookingconfirmed_confirmation_email(user, booking)
     @booking = booking
     @user = user
 
-    mail(to: @user.email, subject: 'Booking confirmed')
+    mail(to: @user.email, subject: 'Booking confirmed', from: "Game-On")
   end
 
   def send_user_bookingrejected_confirmation_email(user, booking)
     @booking = booking
     @user = user
 
-    mail(to: @user.email, subject: 'Booking rejected')
+    mail(to: @user.email, subject: 'Booking rejected', from: "Game-On")
   end
 end
 
